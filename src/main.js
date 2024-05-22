3012a99d0d978ac3da3f1b00a85d03e7146986a4
soir20 <71418127+soir20@users.noreply.github.com>
2024-05-21 22:58:01 -0400

    Remove unused demo JS

--- src/main.js
+++ src/main.js
@@ -35,20 +35,3 @@ function changeTab(tabName) {
 
   activeTabName = tabName;
 }
-
-let greetInputEl;
-let greetMsgEl;
-
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
