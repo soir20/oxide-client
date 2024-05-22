5b2b359a056a4ceafd395dffd11580d671a529bb
soir20 <71418127+soir20@users.noreply.github.com>
2024-05-22 01:39:58 -0400

    Add internationalization

---
+++ src/i18n.js
@@ -0,0 +1,7 @@
+export const LANGUAGES = {
+    "en_US": {
+        "tab_name_saved_servers": "My Servers",
+        "tab_name_settings": "Settings",
+        "saved_servers_scam_warning": "<b>Never join a server that includes paid items.</b> It's a scam! Look for another server where you can earn everything for free."
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri
 
-const tabPrefix = "tab-";
-const tabContentPrefix = "tab-content-";
+// Tab functionality
+const tabPrefix = "tab-"
+const tabContentPrefix = "tab-content-"
 
-let activeTabName = "servers";
+let activeTabName = "servers"
 for (const tab of document.querySelectorAll(".tab")) {
   tab.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (!tab.id.startsWith(tabPrefix)) {
       throw Error(`Tab ID ${tab.id} is missing tab prefix "${tabContentPrefix}"`)
     }
@@ -15,23 +16,46 @@ for (const tab of document.querySelectorAll(".tab")) {
 }
 
 function changeTab(tabName) {
-  const oldTab = document.querySelector(`#${tabPrefix}${activeTabName}`);
-  oldTab.classList.remove(`${tabPrefix}active`);
+  const oldTab = document.querySelector(`#${tabPrefix}${activeTabName}`)
+  oldTab.classList.remove(`${tabPrefix}active`)
 
-  const oldContent = document.querySelector(`#${tabContentPrefix}${activeTabName}`);
+  const oldContent = document.querySelector(`#${tabContentPrefix}${activeTabName}`)
   if (!oldContent) {
-    throw new Error(`Tab ${activeTabName} is missing content`);
+    throw new Error(`Tab ${activeTabName} is missing content`)
   }
-  oldContent.classList.remove(`${tabContentPrefix}active`);
+  oldContent.classList.remove(`${tabContentPrefix}active`)
 
-  const newTab = document.querySelector(`#${tabPrefix}${tabName}`);
-  newTab.classList.add(`${tabPrefix}active`);
+  const newTab = document.querySelector(`#${tabPrefix}${tabName}`)
+  newTab.classList.add(`${tabPrefix}active`)
 
-  const newContent = document.querySelector(`#${tabContentPrefix}${tabName}`);
+  const newContent = document.querySelector(`#${tabContentPrefix}${tabName}`)
   if (!newContent) {
-    throw new Error(`Tab ${tabName} is missing content`);
+    throw new Error(`Tab ${tabName} is missing content`)
   }
-  newContent.classList.add(`${tabContentPrefix}active`);
+  newContent.classList.add(`${tabContentPrefix}active`)
 
-  activeTabName = tabName;
+  activeTabName = tabName
 }
+
+// Internationalization
+import { LANGUAGES } from './i18n.js'
+function loadI18n(langId) {
+  if (!(langId in LANGUAGES)) {
+    throw new Error(`Unknown language ${langId}`)
+  }
+
+  for (const elm of document.querySelectorAll(".i18n")) {
+    const key = elm.getAttribute("data-i18n-key")
+    if (!key) {
+      throw new Error(`Element ${elm.localName} (id: ${elm.id}) is missing i18n key`)
+    }
+
+    if (!LANGUAGES[langId][key]) {
+      throw new Error(`Unknown i18n key ${key} for language ${langId}`)
+    }
+
+    elm.innerHTML = LANGUAGES[langId][key]
+  }
+}
+
+loadI18n("en_US")
